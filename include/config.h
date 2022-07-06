8ad78d592cc365cc5c5d2ac327720e0552e69df6
strah19 <m.strahinja@yahoo.com>
2022-07-05 23:26:26 -0500

    first commit

---
+++ include/config.h
@@ -0,0 +1,4 @@
+// the configured options and settings for Polaris
+#define POLARIS_VERSION_MAJOR 1
+#define POLARIS_VERSION_MINOR 0
+#define USE_VM
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "common.h"
+#include "config.h"
+
+int main(int argc, char* argv[]) {
+    printf("Running Polaris Version %d.%d\n", POLARIS_VERSION_MAJOR, POLARIS_VERSION_MINOR);
+    return 0;
+}
